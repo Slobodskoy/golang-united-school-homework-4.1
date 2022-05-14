d2fc75801573988ea6d9dc3afe1040d5a44f8a77
Slobodskoy <slobodskoy.a.v@yandex.ru>
2022-05-14 19:27:53 +0500

    First

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,12 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	runes := []rune(input)
+	l := len(runes)
+	reverse := make([]rune, l)
+	for inx, r := range runes {
+		reverse[l-inx-1] = r
+	}
+	output = string(reverse)
 	return output
 }
